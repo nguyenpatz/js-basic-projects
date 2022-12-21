c93644b767a84860b6d7a0b1e879e59fe92bfa62
nguyenpatz <nguyenpatzz@gmail.com>
2022-12-21 10:57:50 +0700

    finish simple counter project

---
+++ 02-simple-counter/app.js
@@ -0,0 +1,26 @@
+const mainDisplay = document.querySelector(".main-display");
+const btnDecrease = document.getElementById("decrease");
+const btnReset = document.getElementById("reset");
+const btnIncrease = document.getElementById("increase");
+
+let valueRemember = parseInt(mainDisplay.textContent);
+
+btnIncrease.addEventListener("click", function() {
+    valueRemember++;
+    mainDisplay.textContent = valueRemember;
+    mainDisplay.style.color = "green";
+});
+
+btnDecrease.addEventListener("click", function() {
+    valueRemember--;
+    mainDisplay.textContent = valueRemember;
+    mainDisplay.style.color = "red";
+
+});
+
+btnReset.addEventListener("click", function() {
+    valueRemember = 0;
+    mainDisplay.textContent = valueRemember;
+    mainDisplay.style.color = "#000";
+
+});
